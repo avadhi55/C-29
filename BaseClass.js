02d085a81d4a10296042ffd8f6eb2530da78ef80
Avadhi <avadhijain2118@gmail.com>
2021-04-20 18:58:47 +0530

    Complete the slingshot

--- BaseClass.js
+++ BaseClass.js
@@ -13,11 +13,11 @@ class BaseClass{
       }
       display(){
         var angle = this.body.angle;
-        push();
+        push(); //add a new setting
         translate(this.body.position.x, this.body.position.y);
         rotate(angle);
         imageMode(CENTER);
         image(this.image, 0, 0, this.width, this.height);
-        pop();
+        pop(); //removes the previous setting
       }
 }
--- Slingshot.js
+++ Slingshot.js
@@ -8,6 +8,10 @@ class SlingShot{
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
+        this.image1 = loadImage("sprites/sling1.png");
+        this.image2 = loadImage("sprites/sling2.png");
+        this.image3 = loadImage("sprites/sling3.png");
+
         World.add(world, this.sling);
     }
 
@@ -16,11 +20,28 @@ class SlingShot{
     }
 
     display(){
+        //slingshot images
+        image(this.image1,200,70,40,150);
+        image(this.image2,172,74,40,80);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            push();
+            strokeWeight(8);
+            stroke("rgb(48,22,8)");
+            //bird is on the left of the catapult
+            if(pointA.x<220){
+                line(pointA.x-25, pointA.y, pointB.x-20, pointB.y+20);
+                line(pointA.x-25, pointA.y, pointB.x+25, pointB.y+15);
+                image(this.image3,pointA.x-30,pointA.y-10,15,30);
+
+            }else{
+                line(pointA.x+25, pointA.y, pointB.x-20, pointB.y+20);
+                line(pointA.x+25, pointA.y, pointB.x+25, pointB.y+15);
+                image(this.image3,pointA.x+25,pointA.y-10,15,30);
+            }
+            pop();
+
         }
     }
     
